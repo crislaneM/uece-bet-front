d1955af2a216d30a609b81e829262033902e0767
Thiago Lima <thiagolm46@gmail.com>
2023-11-28 20:24:09 -0300

    ajuste-login

--- bet-front/src/AppRouter.js
+++ bet-front/src/AppRouter.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login/login';
 import Cadastro from './components/Cadastro/cadastro';
 import RecSenha from './components/RecSenha/RecSenha';
 import Main from './components/Main/index';
 
 const AppRouter = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // Verificar se o usuário está autenticado (por exemplo, verificando se há um token JWT no localStorage)
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(!!token);
+  }, []);
+
   return (
     <Router>
-        <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/recuperarSenha" element={<RecSenha />}/>
-        </Routes>
+      <Routes>
+        <Route path="/login" element={<Login onLogin={() => setIsAuthenticated(true)} />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/recuperarSenha" element={<RecSenha />} />
+        <Route
+          path="/"
+          element={isAuthenticated ? <Main /> : <Navigate to="/login" />}
+        />
+      </Routes>
     </Router>
   );
 };
--- bet-front/src/components/Login/login.js
+++ bet-front/src/components/Login/login.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import StdInput from '../Util/StdInput';
 import StdBtn from '../Util/StdBtn';
 import StdTitle from '../Util/Title';
 import styled from 'styled-components';
 import axios from 'axios';
-
 
 const LinksRow = styled.div`
   display: flex;
@@ -14,21 +13,22 @@ const LinksRow = styled.div`
 `;
 
 const Login = () => {
+  const navigate = useNavigate(); // useNavigate hook for programmatic navigation
   const [email, setemail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
   const [emailError, setemailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
 
   const handleLogin = async (event) => {
-    // Simple validation example
     event.preventDefault();
+
     if (!email) {
       setemailError('Campo de preenchimento obrigatório');
-    }
-    else if(!email.includes('@')){
+    } else if (!email.includes('@')) {
       setemailError('Email Inválido: Necessário @');
-    }
-    else {
+    } else {
       setemailError('');
     }
 
@@ -38,74 +38,78 @@ const Login = () => {
       setPasswordError('');
     }
 
-    // If there are errors, don't proceed with login
     if (emailError || passwordError) {
       return;
     }
 
-    // Authentication logic here
-    else {
-      try {
-        const response = await axios.post('http://127.0.0.1:5000/usuario/login', {
-          email: email,
-          senha: password,
-        });
+    setLoading(true);
 
-        console.log(response.data); // Exibindo a resposta do servidor
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/usuario/login', {
+        email: email,
+        senha: password,
+      });
 
-        // Lógica para manipular a resposta conforme necessário
+      const token = response.data.tokens.access;
+      localStorage.setItem('token', token);
 
-      } catch (error) {
-        console.error('Erro:', error);
-      }
+      setSuccessMessage('Login bem-sucedido!');
 
+      // Use navigate to redirect after login
+      navigate('/Main');
+    } catch (error) {
+      console.error('Erro:', error);
+      setSuccessMessage('');
+      // Handle specific error messages as needed
+    } finally {
+      setLoading(false);
     }
-     
   };
 
   return (
     <>
-    <div>
-      <StdTitle />
-      <form>
-        <StdInput
-          label="Email"
-          value={email}
-          onChange={(e) => setemail(e.target.value)}
-        />
-        {emailError && <p className='error'>{emailError}</p>}
-        <StdInput
-          label="Senha"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {passwordError && <p className='error'>{passwordError}</p>}
-        <StdBtn label="Entrar" onClick={handleLogin} mgBotton={'20px'} />
-      </form>
-      <LinksRow>
-        <Link to="/cadastro" style={{ color: 'white' }}>
-          Cadastre-se
-        </Link>
-        <Link to="/recuperarSenha" style={{ color: 'white' }}>
-          Esqueci a senha
-        </Link>
-      </LinksRow>
-    </div>
-    <style type="text/css">
-            {`
+      <div>
+        <StdTitle />
+        <form>
+          <StdInput
+            label="Email"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+          />
+          {emailError && <p className='error'>{emailError}</p>}
+          <StdInput
+            label="Senha"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {passwordError && <p className='error'>{passwordError}</p>}
+          {successMessage && <p className='success'>{successMessage}</p>}
+          <StdBtn label={loading ? 'Carregando...' : 'Entrar'} onClick={handleLogin} mgBotton={'20px'} />
+        </form>
+        <LinksRow>
+          <Link to="/cadastro" style={{ color: 'white' }}>
+            Cadastre-se
+          </Link>
+          <Link to="/recuperarSenha" style={{ color: 'white' }}>
+            Esqueci a senha
+          </Link>
+        </LinksRow>
+      </div>
+      <style type="text/css">
+        {`
+          .error {
+            font-size: 15px;
+            color: red;
+          }
 
-                .error{
-                  font-size: 15px;
-                  color: red;
-                }
-
-                `}
-        </style>
+          .success {
+            font-size: 15px;
+            color: green;
+          }
+        `}
+      </style>
     </>
-    
-    
-    
   );
 };
 
